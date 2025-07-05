b3c66356ebbf356b67ad7ee54ff10e25583119b1
Vishal Jha <visual19852004@gmail.com>
2025-07-05 10:36:40 +0530

    Added some functionalities to the first top bar.

---
+++ src/TopHeader/bellIconWIthNoti.tsx
@@ -0,0 +1,65 @@
+import { GoBell } from "react-icons/go";
+import { futureUpdates } from "./futureUpdates";
+import { useState } from "react";
+
+export const BellIcon = () => {
+  const [showNotification, setShowNotification] = useState(false);
+  return (
+    <div
+      className="relative"
+      onClick={() => setShowNotification(!showNotification)}
+    >
+      <span
+        className="absolute -top-2.5 -right-3 inline-flex items-center justify-center px-1.5 py-1
+          border-2 border-white min-w-[1.25rem] text-xs font-bold leading-none text-[#F6F6F6] bg-[#4B6A4F] rounded-full"
+      >
+        {futureUpdates.length}
+      </span>
+      <GoBell className="size-6 cursor-pointer" />
+      {showNotification && (
+        <div className="absolute top-7 -left-60 border border-[#4B6A4F]">
+          <div
+            className="bg-teal-100 border-t-4 w-80 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
+            role="alert"
+          >
+            <div className="flex">
+              <div className="py-1">
+                <svg
+                  className="fill-current h-6 w-6 text-teal-500 mr-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+                </svg>
+              </div>
+              <div>
+                {
+                  <div>
+                    {futureUpdates.map((update, idx) => (
+                      <div key={idx} className="mb-2">
+                        <p className="font-semibold">{update.title}</p>
+                        {update.priority == "Medium" ? (
+                          <span className="text-sm rounded-lg text-white px-1.5 py-0.5  bg-yellow-600">
+                            {update.priority}
+                          </span>
+                        ) : update.priority == "High" ? (
+                          <span className="text-sm rounded-lg text-white px-1.5 py-0.5 bg-red-500">
+                            {update.priority}
+                          </span>
+                        ) : (
+                          <span className="text-sm rounded-lg text-white px-1.5 py-0.5 bg-green-700">
+                            {update.priority}
+                          </span>
+                        )}
+                      </div>
+                    ))}
+                  </div>
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
--- src/TopHeader/first-header.tsx
+++ src/TopHeader/first-header.tsx
@@ -1,17 +1,25 @@
 import { FaAngleRight } from "react-icons/fa6";
 import { BsThreeDots } from "react-icons/bs";
-import { GoBell } from "react-icons/go";
+import { BellIcon } from "./bellIconWIthNoti";
 import { FiSearch } from "react-icons/fi";
 import { BiSolidDockRight } from "react-icons/bi";
-import { futureUpdates } from "./futureUpdates";
-import { useState } from "react";
 const FirstSpreadsheetHeader = () => {
-  const [showNotification, setShowNotification] = useState(false);
+  type User = {
+    email: string;
+    userName: string;
+  };
+
+  const user: User = {
+    email: "vishal123@gmail.com".slice(0, 8),
+    userName: "Vishal Jha",
+  };
 
   return (
+    // first top bar
     <div className="w-full bg-white border-b border-[#EEEEEE] px-4 py-2 flex justify-between items-center shadow-sm">
       <div className="gap-3">
         <div className="text-sm flex">
+          {/*left side design*/}
           <div className="pr-3">
             <BiSolidDockRight className="size-5 text-[#618666]" />
           </div>
@@ -24,7 +32,7 @@ const FirstSpreadsheetHeader = () => {
         </div>
       </div>
 
-      {/* Right Side Actions */}
+      {/* right side design */}
       <div className="flex gap-5 items-center text-sm">
         <div className="relative">
           <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -36,64 +44,17 @@ const FirstSpreadsheetHeader = () => {
             placeholder="Search within sheet"
           />
         </div>
-        <div
-          className="relative"
-          onClick={() => setShowNotification(!showNotification)}
-        >
-          <span
-            className="absolute -top-2.5 -right-3 inline-flex items-center justify-center px-1.5 py-1
-          border-2 border-white min-w-[1.25rem] text-xs font-bold leading-none text-red-100 bg-[#4B6A4F] rounded-full"
-          >
-            {futureUpdates.length}
-          </span>
-          <GoBell className="size-6 cursor-pointer" />
-          {showNotification && (
-            <div className="absolute top-7 -left-60 border border-[#4B6A4F]">
-              <div
-                className="bg-teal-100 border-t-4 w-80 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
-                role="alert"
-              >
-                <div className="flex">
-                  <div className="py-1">
-                    <svg
-                      className="fill-current h-6 w-6 text-teal-500 mr-4"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
-                    </svg>
-                  </div>
-                  <div>
-                    {
-                      <div>
-                        {futureUpdates.map((update, idx) => (
-                          <div key={idx} className="mb-2">
-                            <p className="font-semibold">{update.title}</p>
-                            {update.priority == "Medium" ? (
-                              <span className="text-sm rounded-lg text-white px-1.5 py-0.5  bg-yellow-600">
-                                {update.priority}
-                              </span>
-                            ) : update.priority == "High" ? (
-                              <span className="text-sm rounded-lg text-white px-1.5 py-0.5 bg-red-500">
-                                {update.priority}
-                              </span>
-                            ) : (
-                              <span className="text-sm rounded-lg text-white px-1.5 py-0.5 bg-green-700">
-                                {update.priority}
-                              </span>
-                            )}
-                          </div>
-                        ))}
-                      </div>
-                    }
-                  </div>
-                </div>
-              </div>
-            </div>
-          )}
+        <div className="pr-2">
+          <BellIcon />
         </div>
-        <div className="w-8 h-8 rounded-full bg-gray-300 flex items-center justify-center text-xs font-semibold text-white">
-          JD
+        <div className="flex gap-2">
+          <div className="w-8 h-8 rounded-full bg-gray-300 flex items-center justify-center text-md font-semibold text-white">
+            VJ
+          </div>
+          <div>
+            <div className="text-[15px] text-[#121212] ">{user.userName}</div>
+            <div className="-mt-2 text-[#757575]">{user.email + "..."}</div>
+          </div>
         </div>
       </div>
     </div>
